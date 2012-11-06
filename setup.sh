61ba3922f30d522095b7d028b18ab9c3d6c161da
cwood <christopher.wood2@twcable.com>
2012-11-06 08:52:05 -0700

    rearranged some stuff, added git configs, added setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s ~/environment/common/bash_aliases ~/.bash_aliases
+ln -s ~/environment/common/bash_completion ~/.bash_completion
+ln -s ~/environment/common/bash_functions ~/.bash_functions
+ln -s ~/environment/common/bash_ps1 ~/.bash_ps1
+ln -s ~/environment/common/bashrc ~/.bashrc
+
+ln -s ~/environment/common/gitignore ~/.gitignore
+
+ln -s ~/environment/work/gitconfig ~/.gitconfig
